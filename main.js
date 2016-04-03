25f13228d9b2088435142c3272bf7814a6f40338
jondcoleman <jcoleman3307@gmail.com>
2016-04-03 17:02:16 -0400

    bluebirdization in process

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 /* eslint strict:0 */
 'use strict'
 
-const inquirer = require('inquirer')
+const Promise = require('bluebird')
+const inquirer = Promise.promisifyAll(require('inquirer'))
 const open = require('open')
 const fileIndex = require('./modules/fileIndex')
 const fileSearch = require('./modules/fileSearch')
@@ -38,28 +39,19 @@ const fileResultQuestion = [{
   message: 'Which file do you want? (Type "cancel" to start over)'
 }]
 
-function initialPrompt() {
-  inquirer.prompt(initialQuestion, (answer) => {
-    if (answer.action === 3) return
-    else if (answer.action === 2) {
-      console.log('Rebuilding.  Please wait...')
-      fileIndex.create(() => {
-        console.log('Done rebuilding')
-        initialPrompt()
-      })
-    } else if (answer.action === 1) {
-      inquirer.prompt(queryQuestion, (answers) => {
-        fileSearch.search(answers.query, (fileList) => {
-          let fileNumber = 0
-          fileList.forEach((file) => {
-            console.log(`${fileNumber}) ${file.filePath}`)
-            fileNumber++
-          })
+function queryPrompt() {
+  inquirer.promptAsync(queryQuestion)
+    .then((answers) => {
+      fileSearch.search(answers.query, (fileList) => {
+        let fileNumber = 0
+        fileList.forEach((file) => {
+          console.log(`${fileNumber}) ${file.filePath}`)
+          fileNumber++
+        })
 
-          inquirer.prompt(fileResultQuestion, (selection) => {
-            if (selection.fileSelection >= 0
-                && selection.fileSelection < fileNumber
-                && typeof Number(selection.fileSelection) === 'number') {
+        inquirer.promptAsync(fileResultQuestion)
+          .then((selection) => {
+            if (selection.fileSelection >= 0 && selection.fileSelection < fileNumber && typeof Number(selection.fileSelection) === 'number') {
               const file = fileList[selection.fileSelection].filePath.replace(/\//g, '\\\\')
               open(file)
             } else if (selection.fileSelection.toLowerCase() === 'cancel') {
@@ -69,13 +61,33 @@ function initialPrompt() {
             }
             initialPrompt()
           })
-        })
       })
-    } else {
-      console.log('invalid command')
-      initialPrompt()
-    }
-  })
+    })
+}
+
+function initialPrompt() {
+  console.log(inquirer.prompt.toString())
+  console.log(inquirer.promptAsync.toString())
+  inquirer.promptAsync(initialQuestion,(a) => console.log(a))
+    .then((answer) => {
+      if (answer.action === 3) {
+        return
+      } else if (answer.action === 2) {
+        console.log('Rebuilding.  Please wait...')
+        fileIndex.create(() => {
+          console.log('Done rebuilding')
+          initialPrompt()
+        })
+      } else if (answer.action === 1) {
+        queryPrompt()
+      } else {
+        console.log('invalid command')
+        initialPrompt()
+      }
+    })
+    .catch((err) => {
+      throw err
+    })
 }
 
 initialPrompt()
